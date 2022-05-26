81afd7293fb1d66cf06fa7128cefe53594f7dace
wfxronald <cool.starmon@gmail.com>
2022-05-26 22:46:07 +0800

    Revise model.py

--- model/model.py
+++ model/model.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
 train_df=pd.read_csv('train.csv')
 test_df=pd.read_csv('test.csv')
 
@@ -9,26 +12,11 @@ Y_train=train_df['Activity']
 X_test=test_df.drop('Activity',axis=1)
 Y_test=test_df['Activity']
 
-from sklearn.linear_model import LogisticRegression
-# lr_clf=LogisticRegression(C=0.01,solver='liblinear')
-# lr_clf.fit(X_train,Y_train)
-
-# from sklearn.metrics import classification_report,accuracy_score
-# y_hat=lr_clf.predict(X_test)
-# print('accuracy score(test data): \n',accuracy_score(Y_test,y_hat))
-# print('Classification report(test dataset): \n',classification_report(Y_test,y_hat))
-
-# define the stages of the pipeline
-from sklearn.pipeline import Pipeline
-pipeline = Pipeline(steps= [('model', LogisticRegression(C=0.01,solver='liblinear'))])
-
-# fit the pipeline model with the training data                            
+# use a simple logistic regression for this classification problem
+pipeline = Pipeline(steps= [('model', LogisticRegression(C=0.01,solver='liblinear'))])                     
 pipeline.fit(X_train, Y_train)
 
-print(pipeline.predict(X_test))
 
-# import joblib
-# from joblib import dump
-
-# # dump the pipeline model
-# dump(pipeline, filename="human_activity_classification.joblib")
+# dump the pipeline model to be used in the front-end
+from joblib import dump
+dump(pipeline, filename="human_activity_classification.joblib")

5763cbe8abba8bb76d719b9a0884b6a03a51967a
Ronald Santoso <ronald.santoso@shopee.com>
2022-06-09 00:51:42 +0800

    Remove hardcoded server url in axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import { SERVER_URL } from './env.js';
 
+var baseUrl = process.env.baseURL || SERVER_URL
 const instance = axios.create({
-	baseURL: SERVER_URL
+	baseURL: baseUrl
 });
 
 export default instance;
